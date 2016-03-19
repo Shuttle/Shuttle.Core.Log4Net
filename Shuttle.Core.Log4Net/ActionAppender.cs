060c02fff1e675a559d69832e527e3ba2487be51
Eben <me@ebenroux.co.za>
2016-03-19 10:15:43 +0200

    - renamed project

---
+++ Shuttle.Core.Log4Net/ActionAppender.cs
@@ -0,0 +1,40 @@
+using System;
+using log4net;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Repository.Hierarchy;
+using Shuttle.Core.Infrastructure;
+
+namespace Shuttle.Core.Log4Net
+{
+	public class ActionAppender : AppenderSkeleton
+	{
+		private readonly Action<LoggingEvent> action;
+
+		public static void Register(Action<LoggingEvent> action)
+		{
+			Guard.AgainstNull(action, "action");
+
+			Log.Trace(string.Format("Registering ActionAppender against action '{0}'.", action.GetType().FullName));
+
+			((Hierarchy)LogManager.GetRepository()).Root.AddAppender(new ActionAppender(action));
+		}
+
+		public ActionAppender(Action<LoggingEvent> action)
+		{
+			Guard.AgainstNull(action, "action");
+
+			this.action = action;
+		}
+
+		protected override void Append(LoggingEvent loggingEvent)
+		{
+			if (loggingEvent == null)
+			{
+				return;
+			}
+
+			action.Invoke(loggingEvent);
+		}
+	}
+}
